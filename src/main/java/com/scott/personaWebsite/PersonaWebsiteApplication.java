05e91d44e8b14ecc852f11ba38eaf8e2b11c67cd
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2024-08-25 23:46:49 -0500

    Initial Spring Boot setup

---
+++ src/main/java/com/scott/personaWebsite/PersonaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.scott.personaWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaWebsiteApplication.class, args);
+	}
+
+}
