e893d49019c8d482729981f91480ee1ccf050bbd
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2024-12-06 15:39:23 -0600

    created dev and prod profiles

--- backend/src/main/java/com/scottphebert/personalwebsite/config/CorsConfig.java
+++ backend/src/main/java/com/scottphebert/personalwebsite/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.scottphebert.personalwebsite.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowedOrigin}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("http://localhost:5173")
+                        //todo look into how to set this up as an environment variable to work across development and prod
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
