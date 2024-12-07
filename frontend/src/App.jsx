9a40ceaa2c77f9f99cb298f2a6f2aaaad69aa2db
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2024-12-07 00:11:21 -0600

    added route guarding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage';
 import HomePage from './components/pages/HomePage'
 import * as Constants from './constants/constants'
+import ProtectedRoutes from './utils/ProtectedRoutes'
 
 
 const App = () =>{
   return (
     <Router>
       <Routes>
-        <Route path={Constants.LANDING_PAGE} element={<LandingPage/>}/>
-        <Route path={"/home"} element={<HomePage/>}/>
+        <Route path={Constants.LANDING_PAGE} element={<LandingPage/>}/>\
+        <Route element={<ProtectedRoutes/>}>
+          <Route path={"/home"} element={<HomePage/>}/>
+        </Route>
       </Routes>
     </Router>
   )
