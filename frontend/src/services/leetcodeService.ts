32af6dcda2f26a5d721a95e9d1cee02ad6617a88
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2025-01-10 08:54:49 -0600

    added timestamps

--- frontend/src/services/leetcodeService.ts
+++ frontend/src/services/leetcodeService.ts
@@ -3,18 +3,13 @@ import handleError from "../utils/ErrorHandler";
 import * as Constants from "../constants/constants"
 
 const getlinkData = async(attempts = 3,) =>{
-    const delay = 1000;
+    const timestamp = new Date().getTime();
     try {
-        const response = await axios.get(Constants.LEETCODE_FILENAME, { responseType: 'text' })
+        const response = await axios.get(Constants.LEETCODE_FILENAME?t=${timestamp}, { responseType: 'text' })
         return response;
     } 
     catch (error) {
-        if(attempts > 0){
-            await new Promise((resolve) => setTimeout(resolve, delay));
-            return getlinkData(attempts - 1);
-        }
-        else
-           return handleError(error, Constants.ERROR_RETRIEVING_LINKS);
+        return handleError(error, Constants.ERROR_RETRIEVING_LINKS);
     }
 }
 
