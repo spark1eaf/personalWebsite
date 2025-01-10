376d4468a26516d9b883bc9bbe7ad60b44f79724
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2025-01-10 08:56:02 -0600

    added timestamps

--- frontend/src/services/leetcodeService.ts
+++ frontend/src/services/leetcodeService.ts
@@ -5,7 +5,7 @@ import * as Constants from "../constants/constants"
 const getlinkData = async(attempts = 3,) =>{
     const timestamp = new Date().getTime();
     try {
-        const response = await axios.get(Constants.LEETCODE_FILENAME?t=${timestamp}, { responseType: 'text' })
+        const response = await axios.get(`${Constants.LEETCODE_FILENAME}?t=${timestamp}`, { responseType: 'text' })
         return response;
     } 
     catch (error) {
