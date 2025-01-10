1ba3f9b295ef01cb3f3cb0c8705b68ac88a1ba9b
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2025-01-10 08:36:46 -0600

    add reattempts for fetching leetcode data

--- frontend/src/services/leetcodeService.ts
+++ frontend/src/services/leetcodeService.ts
@@ -2,13 +2,19 @@ import axios from "axios";
 import handleError from "../utils/ErrorHandler";
 import * as Constants from "../constants/constants"
 
-const getlinkData = async() =>{
+const getlinkData = async(attempts = 3,) =>{
+    const delay = 1000;
     try {
         const response = await axios.get(Constants.LEETCODE_FILENAME, { responseType: 'text' })
         return response;
     } 
     catch (error) {
-        return handleError(error, Constants.ERROR_RETRIEVING_LINKS);
+        if(attempts > 0){
+            await new Promise((resolve) => setTimeout(resolve, delay));
+            return getlinkData(attempts - 1);
+        }
+        else
+           return handleError(error, Constants.ERROR_RETRIEVING_LINKS);
     }
 }
 
