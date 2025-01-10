2762a3f1db39c0b4003600fae8f4e205cfe1739f
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2025-01-10 02:46:59 -0600

    disabled wordle style for prod test

--- frontend/src/components/windows/wordle/WordleAttempt.tsx
+++ frontend/src/components/windows/wordle/WordleAttempt.tsx
@@ -5,12 +5,13 @@ const WordleAttempt = ({attempt}: {attempt:WordleResponse}) =>{
         <div className="attempt-container">
         {attempt.response && attempt.response.length > 0 ?
             attempt.response.map((element, index) =>{
-                const letterStyle:CSSProperties = element.matchType !== undefined ? {
-                    backgroundColor: element.matchType === "partial" ? '#fffb07' : element.matchType === "full" ? '#28a745': 'white'
-                } :{};
+                // const letterStyle:CSSProperties = element.matchType !== undefined ? {
+                //     backgroundColor: element.matchType === "partial" ? '#fffb07' : element.matchType === "full" ? '#28a745': 'white'
+                // } :{};
         
                 return(
-                    <div key={index} style={letterStyle} className="letter-block-cont">
+                    // <div key={index} style={letterStyle} className="letter-block-cont">
+                    <div key={index} className="letter-block-cont">
                         <p>{element.letter.toUpperCase()}</p>
                     </div>
                 )
